43c1e3a846d294257df3ab681da20f41a4a15a8c
Patrick Castro <patrickcastro117@yahoo.com>
2020-07-21 17:14:12 -0700

    added axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World! =)</h1>
-    </div>
-  );
+class FilmPoster extends Component {
+  state = {
+    filmPoster: {}
+  }
+
+  componentDidMount () {
+    axios.get('http://www.omdbapi.com/?apikey=7d40e9d8&i=tt2294629')
+    .then(res => res.data)
+    .then(res => {
+      this.setState = {filmPoster: res};
+    });
+  }
+
+
+
+  // Axios React tutorial
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Cinema Hunter</h1>
+        <input type="text" placeholder="Film Name"/>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default FilmPoster;
