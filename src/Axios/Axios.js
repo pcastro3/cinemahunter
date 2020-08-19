9810e2fcca68a3e9fe24dbd6b10ce5dc66ac045a
Patrick Castro <patrickcastro117@yahoo.com>
2020-08-19 14:44:50 -0700

    input now fetches film and data

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import '../App.css';
 import axios from 'axios';
-import Input from '../InputHandler/InputHandler';
 
 class FilmPoster extends Component {
 
@@ -10,13 +9,19 @@ class FilmPoster extends Component {
     search: ''
   }
 
-  componentDidMount () {
+  searchFilm = event => {
+    event.preventDefault();
+
     axios.get(`https://www.omdbapi.com/?apikey=7d40e9d8&t=${this.state.search}`)
-    .then(res => {
-      this.setState({
-        posters: res.data
-      })
-    })
+      .then(res => {
+        this.setState({
+          posters: res.data
+        })
+      });
+
+      if(this.state.posters.Response === "False") {
+        alert('Unable to find Film. Try again =)');
+      }
   }
 
   handleSearch = event => {
@@ -25,18 +30,22 @@ class FilmPoster extends Component {
     })
   }
 
-  // searchFilm = event => {
-  //   event.preventDefault();
-  //
-  //   axios.get(`https://www.omdbapi.com/?apikey=7d40e9d8&t=${this.state.search}`)
-  //   console.log(this.state.search);
-  // }
-
   render() {
-    // console.log(this.state.search);
 
-    const title = this.state.posters.Title;
-    const poster = this.state.posters.Poster;
+    const info = this.state.posters;
+
+    const title = info.Title;
+    const poster = info.Poster;
+    const plot = info.Plot;
+    const score = info.imdbRating;
+    const genre = info.Genre;
+    const rated = info.Rated;
+    const awards = info.Awards;
+    const date = info.Year;
+    const runtime = info.Runtime;
+    const director = info.Director;
+
+    console.log(info);
 
     return (
       <div className="App">
@@ -46,9 +55,17 @@ class FilmPoster extends Component {
           <button type="submit">Submit</button>
         </form>
         <h2>{title}</h2>
+        <h2>{score}</h2>
+        <h3>{director}</h3>
+        <h3>{genre}</h3>
+        <h3>{rated}</h3>
+        <h3>{awards}</h3>
+        <h3>{date}</h3>
+        <h3>{runtime}</h3>
         <img src={poster} alt=""/>
+        <p>{plot}</p>
       </div>
-    );
+    )
   }
 }
 
